fd6a55dc57f9aec99ad131217db28f143ec9988e
sebastienjean <baz.jean@gmail.com>
2015-10-04 11:10:19 +0200

    Extracted a method (SRP)
    - updateQualityAndSellInForAllItems method has no longer to know
    details about item update, but only to iterate over all shop items and
    ask for item update

--- src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
+++ src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
@@ -33,15 +33,72 @@ public class GildedRose
 		{
 			Item item = items.get(i);
 			
-			if ((!"Aged Brie".equals(item.getName()))
-					&& !"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+			updateItemQualityAndSellIn(item);
+		}
+	}
+
+	private static void updateItemQualityAndSellIn(Item item)
+	{
+		if ((!"Aged Brie".equals(item.getName()))
+				&& !"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+		{
+			if (item.getQuality() > 0)
 			{
-				if (item.getQuality() > 0)
+				if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
 				{
-					if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+					item.setQuality(item.getQuality() - 1);
+				}
+			}
+		}
+		else
+		{
+			if (item.getQuality() < 50)
+			{
+				item.setQuality(item.getQuality() + 1);
+
+				if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+				{
+					if (item.getSellIn() < 11)
 					{
-						item.setQuality(item.getQuality() - 1);
+						if (item.getQuality() < 50)
+						{
+							item.setQuality(item.getQuality() + 1);
+						}
 					}
+
+					if (item.getSellIn() < 6)
+					{
+						if (item.getQuality() < 50)
+						{
+							item.setQuality(item.getQuality() + 1);
+						}
+					}
+				}
+			}
+		}
+
+		if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+		{
+			item.setSellIn(item.getSellIn() - 1);
+		}
+
+		if (item.getSellIn() < 0)
+		{
+			if (!"Aged Brie".equals(item.getName()))
+			{
+				if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
+				{
+					if (item.getQuality() > 0)
+					{
+						if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
+						{
+							item.setQuality(item.getQuality() - 1);
+						}
+					}
+				}
+				else
+				{
+					item.setQuality(item.getQuality() - item.getQuality());
 				}
 			}
 			else
@@ -49,58 +106,6 @@ public class GildedRose
 				if (item.getQuality() < 50)
 				{
 					item.setQuality(item.getQuality() + 1);
-
-					if ("Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
-					{
-						if (item.getSellIn() < 11)
-						{
-							if (item.getQuality() < 50)
-							{
-								item.setQuality(item.getQuality() + 1);
-							}
-						}
-
-						if (item.getSellIn() < 6)
-						{
-							if (item.getQuality() < 50)
-							{
-								item.setQuality(item.getQuality() + 1);
-							}
-						}
-					}
-				}
-			}
-
-			if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
-			{
-				item.setSellIn(item.getSellIn() - 1);
-			}
-
-			if (item.getSellIn() < 0)
-			{
-				if (!"Aged Brie".equals(item.getName()))
-				{
-					if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.getName()))
-					{
-						if (item.getQuality() > 0)
-						{
-							if (!"Sulfuras, Hand of Ragnaros".equals(item.getName()))
-							{
-								item.setQuality(item.getQuality() - 1);
-							}
-						}
-					}
-					else
-					{
-						item.setQuality(item.getQuality() - item.getQuality());
-					}
-				}
-				else
-				{
-					if (item.getQuality() < 50)
-					{
-						item.setQuality(item.getQuality() + 1);
-					}
 				}
 			}
 		}

502c7c6f05355f4b43f1515f658b0afc6b92ae1e
sebastienjean <baz.jean@gmail.com>
2015-10-04 11:04:19 +0200

    Renamed a method
    - updateQuality has been more accurately renamed
    updateQualityAndSellInForAllItems

--- src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
+++ src/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRose.java
@@ -24,10 +24,10 @@ public class GildedRose
 		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
 		items.add(new Item("Conjured Mana Cake", 3, 6));
 
-		updateQuality();
+		updateQualityAndSellInForAllItems();
 	}
 
-	public static void updateQuality()
+	public static void updateQualityAndSellInForAllItems()
 	{
 		for (int i = 0; i < items.size(); i++)
 		{

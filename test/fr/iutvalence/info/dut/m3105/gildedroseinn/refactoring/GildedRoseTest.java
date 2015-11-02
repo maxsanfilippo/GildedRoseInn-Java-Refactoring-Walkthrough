70973c9966abc6014636792925c30093ee58bc43
Sanfilippo <max.sanfilippo@iut-valence.fr>
2015-11-02 09:25:07 +0100

    Test 2 Red

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -21,5 +21,16 @@ public class GildedRoseTest
 		assertEquals(items.get(0).getSellIn(), 2);
 		assertEquals(items.get(0).getQuality(), 5);
 	}
+	
+	@Test
+	public void updateQualitySellinForItemsShouldDecrementTwiceQualityWhenSellinNegative()
+	{
+		GildedRose gilded = new GildedRose();
+		List <Item> items = new ArrayList<Item>();
+		items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+		gilded.updateQualityAndSellInForAllItems(items);
+		assertEquals(items.get(0).getSellIn(), -1);
+		assertEquals(items.get(0).getQuality(), 78);
+	}
 
 }
